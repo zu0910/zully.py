11b7f0332f169d9120495ebc5cf4bd4c8f35f551
zu0910 <zullyortiz.09@gmail.com>
2024-04-16 12:54:26 -0500

    ejercicios_refuerzo

---
+++ Python_OrtizZully/Dia5/Numeros_duplicados.py
@@ -0,0 +1,11 @@
+numeros = []#el usuario ingresa la lista de numeros
+
+
+numbers = list(set(input(numeros)))#set es usado para eliminar los numeros que estan duplicados 
+numbers.sort()#sort ordenara los numeros que agrego a la lista 
+print(numbers)#imprimir todos los numeros egresados sin repeticiones y ordenados 
+
+#Elaborado por Zully Fernanda Ortiz Avendaño Cc. 1092528097
+               
+
+
